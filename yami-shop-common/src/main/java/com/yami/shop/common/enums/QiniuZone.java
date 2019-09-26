a0d58cad8a4715add2d6be7629db14597e8b6350
OPGame <1242479791@qq.com>
2019-09-26 09:43:21 +0800

    根据配置文件选择七牛云机房

---
+++ yami-shop-common/src/main/java/com/yami/shop/common/enums/QiniuZone.java
@@ -0,0 +1,39 @@
+package com.yami.shop.common.enums;
+
+/**
+ * 七牛云zone的选择
+ * @author LGH
+ */
+public enum QiniuZone {
+
+    /**
+     * 华东机房相关
+     */
+    HUA_DONG(),
+
+    /**
+     * 华北机房相关
+     */
+    HUA_BEI(),
+
+    /**
+     * 华南机房相关
+     */
+    HUA_NAN(),
+
+    /**
+     * 北美机房相关
+     */
+    BEI_MEI(),
+
+    /**
+     * 新加坡机房相关
+     */
+    XIN_JIA_PO(),
+
+    ;
+
+    QiniuZone(){
+
+    }
+}
