a3f4409792988dd86c1eff0e92ddf06a2372b442
LGH <1242479791@qq.com>
2020-04-13 15:15:34 +0800

    小程序默认连接127.0.0.1，免得有人不清楚要连接到那个地方

--- mall4m/utils/config.js
+++ mall4m/utils/config.js
@@ -1,11 +1,3 @@
-//可改动页面
-
-var domain = "https://mini-api.gz-yami.com"; //统一接口域名，正式环境
-//  var domain = "http://192.168.0.173:8086"; //统一接口域名，测试环境
-//var domain = "http://192.168.0.153:8286"; //测试环境
-
-// var picDomain = '';//图片域名
+var domain = "http://127.0.0.1:8086"; //统一接口域名，测试环境
 
 exports.domain = domain;
-
-// exports.picDomain = picDomain;
