13a0692989a277cc2e5ba06e3ef673d412a26d68
cl <mumu781536077@163.com>
2021-08-06 08:48:47 +0800

    提示文案修改

--- mall4v/src/views/modules/sys/role.vue
+++ mall4v/src/views/modules/sys/role.vue
@@ -106,7 +106,7 @@ export default {
       var ids = id ? [id] : this.dataListSelections.map(item => {
         return item.roleId
       })
-      this.$confirm(`确定对[id=${ids.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
+      this.$confirm(`确定进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
