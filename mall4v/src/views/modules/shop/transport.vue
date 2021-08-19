46864e207164c26b3dca6ee76f69d373439e1787
cl <mumu781536077@163.com>
2021-08-19 17:24:07 +0800

    修改样式

--- mall4v/src/views/modules/shop/transport.vue
+++ mall4v/src/views/modules/shop/transport.vue
@@ -24,6 +24,7 @@
         <el-button type="danger"
                    @click="deleteHandle()"
                    v-if="isAuth('shop:transport:delete')"
+                   size="small"
                    :disabled="dataListSelections.length <= 0">批量删除
         </el-button>
       </template>
