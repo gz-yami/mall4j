368f62d2b6cce04b8350687538815f676190522e
chendt <18902722133@163.com>
2021-11-10 15:37:49 +0800

    域名替换

--- mall4v/static/config/index.js
+++ mall4v/static/config/index.js
@@ -8,7 +8,7 @@
   window.SITE_CONFIG['baseUrl'] = 'http://192.168.0.143:8085'
 
   // 静态资源文件url
-  window.SITE_CONFIG['resourcesUrl'] = 'https://img.mall4j.com//'
+  window.SITE_CONFIG['resourcesUrl'] = 'https://img.mall4j.com/'
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './' // 域名
   window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
