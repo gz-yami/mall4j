deab9296035768617eac34009e21aeb1fe700899
chendt <18902722133@163.com>
2021-11-10 15:37:31 +0800

    域名替换

--- mall4v/static/config/index-prod.js
+++ mall4v/static/config/index-prod.js
@@ -7,7 +7,7 @@
   // api接口请求地址
   window.SITE_CONFIG['baseUrl'] = 'https://mall4j-admin.mall4j.com/apis'
   // 静态资源文件url
-  window.SITE_CONFIG['resourcesUrl'] = 'https://img-test.mall4j.com/'
+  window.SITE_CONFIG['resourcesUrl'] = 'https://img.mall4j.com//'
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain'] = './' // 域名
   window.SITE_CONFIG['version'] = ''   // 版本号(年月日时分)
