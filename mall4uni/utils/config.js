b164573a502cc1f9363b7e53ed7628c1c3bf353d
FrozenWatermelon <frozenwatermelon@163.com>
2023-02-21 11:14:53 +0800

    默认dev的地址改成127.0.0.1

--- mall4uni/utils/config.js
+++ mall4uni/utils/config.js
@@ -1,28 +1,8 @@
-//可改动页面
-// var domain = "http://127.0.0.1:8086"; //统一接口域名，测试环境
-
-// exports.domain = domain;
-
-
-//配置文件
-// #ifndef MP
-// var domain = "http://mall4j-admin.mall4j.com/api"; //统一接口域名，正式环境
-// var wsDomain = "http://mall4j-admin.mall4j.com/api"; //统一接口域名，正式环境
-// #endif
-
-// #ifndef MP
-// var domain = "https://h5.mall4j.com/api"; //统一接口域名，正式环境
-// var wsDomain = "https://h5.mall4j.com/api"; //统一接口域名，正式环境
-// #endif
-
+// 公众号appid
 var mpAppId = 'wx42bbe857570c5b40'
 // 临时
-var domain = "http://192.168.1.17:8086"; //统一接口域名，测试环境
-var wsDomain = "ws://192.168.1.17:8086"; //统一接口域名，测试环境
+var domain = "http://127.0.0.1:8086"; //统一接口域名，测试环境
 
-var picDomain = "https://img.mall4j.com/"; //图片域名
 
 exports.domain = domain;
-exports.picDomain = picDomain;
-exports.wsDomain = wsDomain;
 exports.mpAppId = mpAppId;
